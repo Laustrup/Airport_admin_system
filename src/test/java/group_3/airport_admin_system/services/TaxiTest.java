682b62e377a11573969c637d6b4e545b9aeb580b
Laust <laust.bonnesen@mail.com>
2021-10-05 14:59:32 +0200

    Start of test

--- src/test/java/group_3/airport_admin_system/services/TaxiTest.java
+++ src/test/java/group_3/airport_admin_system/services/TaxiTest.java
@@ -1,24 +1,45 @@
 package group_3.airport_admin_system.services;
 
+import group_3.airport_admin_system.model.FlightPlan;
+import group_3.airport_admin_system.model.Gate;
 import group_3.airport_admin_system.repositories.FlightPlanRepository;
 import group_3.airport_admin_system.repositories.GateRepository;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaxiTest {
 
+    private FlightPlanRepository fpRepo;
+    private GateRepository gRepo;
+
+    public TaxiTest(FlightPlanRepository fpRepo, GateRepository gRepo) {
+        this.fpRepo = fpRepo;
+        this.gRepo = gRepo;
+    }
+
     @ParameterizedTest
-    @CsvSource(value = "", delimiter = '|')
-    public void movePlaneToGate(int gateNumber, String routeNumber, FlightPlanRepository fpRepo, GateRepository gRepo) {
+    @CsvSource(value = "27|D8 3563|Taxing to gate 27|false", delimiter = '|')
+    public void movePlaneToGate(int gateNumber, String routeNumber,
+                                String expectedGateInfo,boolean expectedAvailability) {
         // Arrange
         Taxi taxi = new Taxi(fpRepo,gRepo);
+        if (expectedGateInfo.equals("null")) {
+            expectedGateInfo = null;
+        }
 
-        // ACt
+        // Act
+        taxi.movePlaneToGate(gateNumber,routeNumber);
 
+        List<FlightPlan> flightPlans = fpRepo.findByRouteNumber(routeNumber);
+        List<Gate> gates = gRepo.findByGateNumber(gateNumber);
 
         // Assert
+        assertEquals(expectedGateInfo,flightPlans.get(0).getGateInfo());
+        assertEquals(expectedAvailability,gates.get(0).isAvailable());
     }
 
 }
