cd01cbda0965e1b38a34d703062a1257283f1681
Laust <laust.bonnesen@mail.com>
2021-10-05 13:06:57 +0200

    Merging entities

--- src/test/java/group_3/airport_admin_system/services/TowerToFlightTest.java
+++ src/test/java/group_3/airport_admin_system/services/TowerToFlightTest.java
@@ -1,5 +1,7 @@
 package group_3.airport_admin_system.services;
 
+import group_3.airport_admin_system.repositories.FlightPlanRepository;
+import group_3.airport_admin_system.repositories.GateRepository;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,8 +11,9 @@ class TowerToFlightTest {
 
     @ParameterizedTest
     @CsvSource(value = "", delimiter = '|')
-    public void movePlaneToGate(int gateNumber, String routeNumber) {
+    public void movePlaneToGate(int gateNumber, String routeNumber, FlightPlanRepository fpRepo, GateRepository gRepo) {
         // Arrange
+        Taxi taxi = new Taxi(fpRepo,gRepo);
 
 
         // ACt
